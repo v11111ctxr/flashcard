2ef4a54922d38fb8aaf3b51635aeff8fc0979639
v11111ctxr <140622711+v11111ctxr@users.noreply.github.com>
2024-10-16 10:41:43 -0300

    Add files via upload

---
+++ FLASHCARD/assets/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'É um mecanismo para adicionar estilos a uma página web'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'É uma linguagem de programação interpretada estruturada'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz pão em Inglês?',
+    'Pão em ingles é BREAD (BRED)'
+)
